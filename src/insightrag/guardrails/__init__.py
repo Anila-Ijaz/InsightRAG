97b63a0a465abca379c5c1beade70f52cde25afd
Anila-Ijaz <anilaijaz1507@gmail.com>
2026-05-18 14:41:59 +0200

    guardrails  added

---
+++ src/insightrag/guardrails/__init__.py
@@ -0,0 +1,4 @@
+from insightrag.guardrails.input_guard import InputGuard, PromptInjectionDetected
+from insightrag.guardrails.output_guard import OutputGuard
+
+__all__ = ["InputGuard", "OutputGuard", "PromptInjectionDetected"]
