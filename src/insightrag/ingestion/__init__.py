aac0e86144ec0683479394b77219b9f954e9075a
Anila-Ijaz <anilaijaz1507@gmail.com>
2026-05-19 15:38:11 +0200

    ingestion  added

---
+++ src/insightrag/ingestion/__init__.py
@@ -0,0 +1,4 @@
+from insightrag.ingestion.chunker import SemanticChunker
+from insightrag.ingestion.parser import SECFilingParser, ParsedDocument
+
+__all__ = ["SECFilingParser", "ParsedDocument", "SemanticChunker"]
